48dcb7345063763a4dd743e717e5239e18f9b4d6
Claudiu <ionel.claudiu394@gmail.com>
2021-05-15 18:41:48 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,8 +29,8 @@ function App() {
       <AppContext.Provider value={globalState}>
         <Router basename="/Pokedex">
           <Switch>
-            <Route exact path="/" component={Homepage} />
-            <Route exact path="/:id" component={PokeDex} />
+            <Route path="/:id" component={PokeDex} />
+            <Route path="/" component={Homepage} />
           </Switch>
         </Router>
       </AppContext.Provider>
